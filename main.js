11bd8ef042830eac26f62eca2f4801fbd2c3e701
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-10 05:58:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-$('#select-menu').on('change',function(){
-let name = $('#select-menu option:selected').text();
-let distance = $('#select-menu option:selected').val();
-let price = $('#select-menu option:selected').data('price');
-if(distace){
-  $('#feedback-message').text('You are signing up for a ' + name + ',which cost ' + ',to a distnce of ' + distance + 'km');
-} else {
-  $('#feedback-message').empty();
-}
+$('#input-name').on('keyup',function(){
+let name = $(this).val();
+$('#feedback-message').text('pleased to meet you, ' + name);
 });
 });
