060de5fba2bbbcb0194286d6209011039041b4b8
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-10 17:17:46 +0100

    Code modified
    The code has been modified to apply the animation effect.

--- main.js
+++ main.js
@@ -16,12 +16,8 @@ html += '<div class="description">Lorem ipsum dolor sit
 html += '<div class="price">499</div>';
 html += '<button>Add to cards</button>';
 html += '<br/>';
-html += '<a class="more-info-link" href = "#">More info</a>';
-html += '<div class="more-info">Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Porro cumque enim
-           quisquam saepe non quos optio, neque labore
-          debitis dolor minus ea molestiae beatae, officiis, 
-          perferendis ex excepturi exercitationem ipsum?
+html += '<a class="more-info-link" href ="#">More info</a>';
+html += '<div class="more-info">Lorem ipsum dolor sit,
          </div>';
 html += '</div>;
 
@@ -31,8 +27,11 @@ $('container').on('click','.more-info-link',function(event)
 {
   event.preventDefault();
 
-  $(this).parent().find('.more-info').toggle();
+  $(this).parent().find('.more-info').fadetoggle(slow);
 });
+$(this)
+.animate({"opacity":0, "margin-left":10}, 'fast')
+.animate({"opacity": 1.0, "margin-left":0}, 'fast');
 
 $('container').on('click','.item-remove', function(){
 $(this).parent().remove();
