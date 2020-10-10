362105e08ed949ccd9a5191a904a863365aa25ff
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-10 07:38:09 +0100

    Modification
    A code to prevent default has been added.

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ $('#input-name').on('keyup',function(){
 let name = $(this).val();
 $('#feedback-message').text('pleased to meet you, ' + name);
 });
+$('a').on('click',function(ev){
+event.preventDefault();
+$('#feedback-message').text('That\'s fine!');
+
+});
 });
