c9beee58d58315b4b6cbfcfbff6dc451ec9ce175
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-10 10:04:41 +0100

    Change of code
    Code modified to add items.

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
-$('#input-name').on('keyup',function(){
-let name = $(this).val();
-$('#feedback-message').text('pleased to meet you, ' + name);
-});
-$('a').on('click',function(ev){
-event.preventDefault();
-$('#feedback-message').text('That\'s fine!');
+$('#buuton-create-item').on('keyup',function(){
+let name = $('#input-create-item').val();
+$('#input-create-item').val('');
+
+let html = '';
+html += <div class="item">
+html += '<div class="name">' + name + '</div>'
+html += '<img src="assets/beach.jpg"></img>';
+html += '<div class="description">Lorem ipsum dolor sit
+             amet consectetur adipisicing elit. 
+             Aperiam assumenda quis tempora aspernatur 
+             voluptate amet nemo. Animi repudiandae impedit, 
+             illo voluptates sunt, laborum doloribus corporis
+              magnam numquam voluptatum ipsam et.</div>';
+html += '<div class="price">499</div>';
+html += '<button>Add to cards</button>';
+html += '<br/>';
+html += '<div href = "#">More info</div>';
+html += '<div class="more-info">Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Porro cumque enim
+           quisquam saepe non quos optio, neque labore
+          debitis dolor minus ea molestiae beatae, officiis, 
+          perferendis ex excepturi exercitationem ipsum?
+         </div>';
+html += '</div>;
+
+$('#container').prepend(html);
 
 });
 });
